7bbe81bd22852b9dbfa4550bfa4de12f17dd5fb4
Joseph Dambakli <josephdambakli@Josephs-iMac.local>
2025-11-07 16:49:28 -0600

    New progress, not much fr fr

---
+++ SentimentApp/SentimentApp/Core/Root/ResponseRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ResponseRowView.swift
+//  SentimentApp
+//
+//  Created by Joseph Dambakli on 10/21/25.
+//
+
+import SwiftUI
+
+struct ResponseRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResponseRowView()
+}
